555260dabdf634f912884e9a7df6c18a1508de6d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-30 19:20:58 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -74,13 +74,13 @@ function makePanel(title, content/* what data does the panel need? */) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div')
+  const panelBar = document.createElement('div')
+  const panelContent = document.createElement('div')
+  const panelTitle = document.createElement('div')
+  const panelButtons = document.createElement('div')
+  const openButton = document.createElement('div')
+  const closeButton = document.createElement('div')
 
 
   // TASK 6- Setup the structure of our elements
